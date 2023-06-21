cc0e962d8da0c767cb4f4597cba999bb964441b5
ayushkumar146 <108751760+ayushkumar146@users.noreply.github.com>
2023-06-21 20:17:52 +0530

    Add files via upload

---
+++ ENCRYPTA/script.js
@@ -0,0 +1,10 @@
+function fill(){
+// let c=document.getElementById('fname').value='ayush';
+// c='ayush';
+let f=document.forms[0];
+f['fname'].value='ayush';
+f['lname'].value='Das';
+f['email'].value='ayush.kumardas146@gmail.com';
+f['pass'].value='ayush';
+f['num'].value='8368460530';
+}
